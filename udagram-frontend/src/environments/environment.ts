9af2a271cccf787b601b7595d474bf958d0d42d9
namdev96 <namdev96.info@gmail.com>
2022-11-06 13:29:21 +0700

    add kubernetes obj and new version frontend

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a45af731356874434923e87a6a0655de-2087597221.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
